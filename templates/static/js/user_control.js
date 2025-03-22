2e92ed8ce548e3efd187822461ea0bcd5d5b3f59
virgil698 <xiaokong23357@163.com>
2025-03-23 03:13:10 +0800

    html 前端优化 （来自 超级苦力怕1324 ）

--- templates/static/js/user_control.js
+++ templates/static/js/user_control.js
@@ -11,17 +11,17 @@ function incrementLike(postId) {
             'Content-Type': 'application/json'
         }
     })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            // 更新点赞计数
-            const likeCountElement = document.querySelector('.like-count');
-            likeCountElement.textContent = `点赞: ${data.like_count}`;
-        } else {
-            alert(data.message);
-        }
-    })
-    .catch(error => console.error('Error:', error));
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                // 更新点赞计数
+                const likeCountElement = document.querySelector('.like-count');
+                likeCountElement.textContent = `点赞: ${data.like_count}`;
+            } else {
+                alert(data.message);
+            }
+        })
+        .catch(error => console.error('Error:', error));
 }
 
 // 定义举报帖子函数
@@ -40,15 +40,15 @@ function reportPost(postId) {
         },
         body: JSON.stringify({ reason })
     })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            alert(data.message);
-        } else {
-            alert(data.message);
-        }
-    })
-    .catch(error => console.error('Error:', error));
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                alert(data.message);
+            } else {
+                alert(data.message);
+            }
+        })
+        .catch(error => console.error('Error:', error));
 }
 
 // 定义评论点赞函数
@@ -60,17 +60,17 @@ function incrementLikeComment(commentId) {
             'Content-Type': 'application/json'
         }
     })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            // 更新评论的点赞计数
-            const likeCountElement = document.querySelector(`.comment-like-count-${commentId}`);
-            likeCountElement.textContent = `点赞: ${data.like_count}`;
-        } else {
-            alert(data.message);
-        }
-    })
-    .catch(error => console.error('Error:', error));
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                // 更新评论的点赞计数
+                const likeCountElement = document.querySelector(`.comment-like-count-${commentId}`);
+                likeCountElement.textContent = `点赞: ${data.like_count}`;
+            } else {
+                alert(data.message);
+            }
+        })
+        .catch(error => console.error('Error:', error));
 }
 
 // 定义举报评论函数
@@ -89,13 +89,25 @@ function reportComment(commentId) {
         },
         body: JSON.stringify({ reason })
     })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            alert(data.message);
-        } else {
-            alert(data.message);
-        }
-    })
-    .catch(error => console.error('Error:', error));
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                alert(data.message);
+            } else {
+                alert(data.message);
+            }
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+let cig_id = document.getElementById("comment-input-group");
+let comment_id = document.getElementById("comment");
+function comment_input_animation() {
+    cig_id.classList.add("a");
+    comment_id.classList.add("a");
+}
+
+function Rcomment_input_animation() {
+    cig_id.classList.remove("a");
+    comment_id.classList.remove("a");
 }
