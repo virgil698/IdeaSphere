15edc4d5ce2e0a3a439c63fc41ec19afa5abe935
virgil698 <xiaokong23357@163.com>
2025-07-02 00:45:21 +0800

    重写ideasphere后端

---
+++ src/main/java/org/ideasphere/ideasphere_Java/IdeasphereJavaApplication.java
@@ -0,0 +1,13 @@
+package org.ideasphere.ideasphere_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeasphereJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeasphereJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ideasphere/ideasphere_Java/IdeasphereJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package ideasphere.ideasphere_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeasphereJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
