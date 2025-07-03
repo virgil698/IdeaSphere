a5c95e2b7e3c6c03390271b1c81b964a81a6641b
virgil698 <xiaokong23357@163.com>
2025-07-03 14:32:35 +0800

    准备更新代码

--- src/main/java/org/ideasphere/ideasphere/IdeaSphereApplication.java
+++ src/main/java/org/ideasphere/ideasphere/IdeaSphereApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IdeaSphereApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(IdeaSphereApplication.class, args);
     }
 
